aafeaf6256098c2f2237c4f052b7f54a1f54ab91
Jeremy Eltho <57611532+JeremyEltho@users.noreply.github.com>
2026-09-15 04:13:11 -0700

    diag check

--- pythonprac/med.py
+++ pythonprac/med.py
@@ -41,7 +41,8 @@ def find_invalid_records(
     constraints = {
         'patient_id': isinstance(patient_id, str) and re.fullmatch('p\d+', patient_id, re.IGNORECASE),
         'age': isinstance(age, int) and age >= 18,
-        'gender':isinstance(gender, str) and gender.lower() in ('male', 'female')
+        'gender': isinstance(gender, str) and gender.lower() in ('male', 'female'),
+        'diagnosis': isinstance(diagnosis,str) or is not
         
     }
     return constraints
